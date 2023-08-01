1908ef4c5d34478bcfa4084de71781548ee0435d
harry05555 <sebastian.schwill@gmail.com>
2023-08-01 20:51:04 +0200

    init

---
+++ src/main/java/de/harry05/BackendDocushark/BackendDocusharkApplication.java
@@ -0,0 +1,13 @@
+package de.harry05.BackendDocushark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDocusharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDocusharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/harry05/BackendDocushark/BackendDocusharkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.harry05.BackendDocushark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDocusharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
